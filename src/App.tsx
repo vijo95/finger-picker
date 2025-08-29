e71acab2ffe77fdc90e3bb2f73218b519d3e7d99
Victor <victor.casulli@pentlandfirth.com>
2025-08-29 13:17:19 +0200

    vibration

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ import { useState, type JSX } from "react";
 import { TouchArea } from "./components/touchArea/touchArea";
 import { Settings } from "./components/settings/settings";
 
+function VibrateButton() {
+  const vibrate = (pattern = [200, 100, 200]) => {
+    if ("vibrate" in navigator) {
+      navigator.vibrate(pattern);
+    } else {
+      console.warn("Vibration API not supported on this device/browser.");
+    }
+  };
+
+  return (
+    <button
+      style={{ position: "absolute", color: "white", top: 10, right: 10 }}
+      onClick={() => vibrate()}
+    >
+      Vibrate
+    </button>
+  );
+}
+
 export default function App(): JSX.Element {
   const [numberOfWinners, setNumberOfWinners] = useState(1);
   const [gameStarted, setGameStarted] = useState(false);
@@ -20,6 +39,7 @@ export default function App(): JSX.Element {
           setNumberOfWinners={setNumberOfWinners}
         />
       )}
+      <VibrateButton />
     </>
   );
 }
