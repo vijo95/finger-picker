1cf48af66134e5a0561241a3780fa6f69c31e729
Victor <victor.casulli@pentlandfirth.com>
2025-09-02 11:49:58 +0200

    double border touch point

--- src/components/touchPoint/touchPoint.tsx
+++ src/components/touchPoint/touchPoint.tsx
@@ -17,10 +17,10 @@ export const WinnerTouchPoint = ({ x, y }: { x: number; y: number }) => {
     <div
       className="genericTouchPoint winnerTouchPoint"
       style={{
-        left: x - 44,
-        top: y - 44,
+        left: x - 60,
+        top: y - 60,
       }}
-    />
+    ></div>
   );
 };
 
